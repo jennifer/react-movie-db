e85dfb4f319fbf6e86961c9f24bddbc00f8bf27e
jennifer kerssen <jjkerssen@gmail.com>
2018-11-30 20:44:49 -0700

    add state, inline conditionals, and onClick event

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    toggle: true
+  }
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Welcome text='welcome to React'/>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          {this.state.toggle && 
+            <p>This should show and hide</p>
+          }
+          <button onClick={this.toggle}>Show/Hide</button>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -22,6 +38,16 @@ class App extends Component {
         </header>
       </div>
     );
+  }
+}
+
+class Welcome extends Component {
+  render(){
+    const { text, toggle } = this.props;
+    console.log(toggle);
+    return (
+      <h1 classsName='App-title'>{text}</h1>
+    )
   }
 }
 

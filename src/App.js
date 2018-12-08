647a462887da55f9ee04cdde87fb682581c46f8a
jennifer kerssen <jjkerssen@gmail.com>
2018-12-08 16:26:51 -0700

    add movie component

--- src/App.js
+++ src/App.js
@@ -1,53 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Movie from './movie';
 import './App.css';
 
+const movies = [{
+    id: 1,
+    title: 'Star Wars'
+  }, {
+    id: 2,
+    title: 'Spider Man'
+  }, {
+    id: 3,
+    title: 'Harry Potter'
+  }
+];
+
 class App extends Component {
-
-  state = {
-    toggle: true
-  }
-
-  toggle = () => {
-    this.setState({
-      toggle: !this.state.toggle
-    })
-  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text='welcome to React'/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          {this.state.toggle && 
-            <p>This should show and hide</p>
-          }
-          <button onClick={this.toggle}>Show/Hide</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie}/>
+        ))}
       </div>
     );
-  }
-}
-
-class Welcome extends Component {
-  render(){
-    const { text, toggle } = this.props;
-    console.log(toggle);
-    return (
-      <h1 classsName='App-title'>{text}</h1>
-    )
   }
 }
 

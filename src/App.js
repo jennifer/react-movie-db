e0b5597f20a856d9d702dfd1f560fbb861c98a5f
jennifer kerssen <jjkerssen@gmail.com>
2018-12-08 18:40:37 -0700

    fetch API data

--- src/App.js
+++ src/App.js
@@ -3,29 +3,63 @@ import logo from './logo.svg';
 import Movie from './movie';
 import './App.css';
 
-const movies = [{
-    id: 1,
-    title: 'Star Wars'
-  }, {
-    id: 2,
-    title: 'Spider Man'
-  }, {
-    id: 3,
-    title: 'Harry Potter'
-  }
-];
-
 class App extends Component {
 
+  state = {
+    movies: [],
+    error: null
+  }
+
+  // With Promises
+  componentDidMount() {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=eb8852fb04f1569c57fd1e56080fd202&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+    .then(res => {
+      if (!res.ok) {
+          return Promise.reject(res.statusText);
+      }
+      return res.json();
+    })
+    .then(movies =>
+      this.setState({
+        movies: movies.results
+      })
+    )
+    .catch(err =>
+      console.log('Whoops')
+      /*
+      // This is where you could set a user-facing message
+      this.setState({
+          error: 'Could not fetch',
+          loading: false
+      })
+      */
+    );
+  }
+  
+  /*
+  // With async/await
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=eb8852fb04f1569c57fd1e56080fd202&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results,
+      });
+    } catch(e) {
+      console.log(e);
+    }
+  }
+*/
+
   render() {
+    console.log(this.state.movies);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => (
-          <Movie key={movie.id} movie={movie}/>
-        ))}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie}/>)}
       </div>
     );
   }
